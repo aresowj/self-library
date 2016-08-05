ace4fcee6b8568bb3e3799cd652000c33518b8fa
Ares Ou <aresowj@gmail.com>
2016-08-05 14:53:33 -0700

    Update get_django_model_classes.py

--- Django/get_django_model_classes.py
+++ Django/get_django_model_classes.py
@@ -1,4 +1,3 @@
-
 def get_django_model_classes(module):
     # getmembers() will return a tuple like (('class_name', class_ref), ()...)
     return [ret[1] for ret in inspect.getmembers(module, inspect.isclass) if issubclass(ret[1], models.Model)]
