dfd7577305d8874ea763a953e86f0d12fdd32ffa
Ares Ou <aresowj@gmail.com>
2016-08-18 11:25:25 -0700

    Create run_coverage.sh
    A shell script to run Python coverage with virtualenv. You can define your own `--omit` arguments and file names.

---
+++ Python/run_coverage.sh
@@ -0,0 +1,9 @@
+old_path=$(PWD)
+old_pythonpath=$(PYTHONPATH)
+. ./venv/bin/activate
+export PYTHONPATH=$PYTHONPATH:$PWD
+cd manager
+coverage run --omit=../venv/*,../lib/* unit_tests.py
+coverage report
+cd "$old_path"
+export PYTHONPATH=$old_pythonpath
