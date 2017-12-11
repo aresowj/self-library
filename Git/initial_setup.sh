e5a2b0357e0501dd86bb24d1e28d5dbafa4165b2
Ares Ou <aresowj@gmail.com>
2017-12-11 15:22:38 -0800

    Script for initializing global git configs

---
+++ Git/initial_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Setup user name and email, used for commits
+git config --global user.name "Ares Ou"
+git config --global user.email "aresowj@gmail.com"
+
+# Setup alias for shortcuts
+git config --global alias.cm "commit -m"
+git config --global alias.co "checkout"
+git config --global alias.br "branch"
+
+# Setup default editor
+git config --global core.editor emacs
