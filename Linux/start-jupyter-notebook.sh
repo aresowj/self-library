ff99273d13dc5c2db39386f2d75a50e42c1ae7a5
Ares Ou <aresowj@outlook.com>
2020-05-25 11:57:58 -0700

    Update start-jupyter-notebook.sh

--- Linux/start-jupyter-notebook.sh
+++ Linux/start-jupyter-notebook.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-exec jupyter notebook &> /home/kagami/jupyter_run.log &
+source /home/kagami/python_env/jupyter/bin/activate
+cd /home/kagami/jupyter
+exec jupyter notebook --ip=0.0.0.0 &> /home/kagami/jupyter_run.log
